d52c89f839ff3facb6b3b5471367c9fa9be4f29c
RaisaNabila12 <raisa35@mhs.usk.ac.id>
2025-12-01 16:49:37 +0700

    Update Penumpang.java

--- Penumpang.java
+++ Penumpang.java
@@ -18,3 +18,55 @@ public class Penumpang {
         this.hamil = hamil;
         this.saldo = SALDO_AWAL; // Saldo awal 10.000 
     }
+
+    // Method Accessor (getters) 
+    // + getID(): int 
+    public int getID() {
+        return id;
+    }
+
+    // Tambahan method getName() untuk mempermudah implementasi method turunkanPenumpang(nama) 
+    public String getNama() {
+        return nama;
+    }
+
+    // + getUmur(): int 
+    public int getUmur() {
+        return umur;
+    }
+
+    // + getHamil(): Boolean 
+    public boolean isHamil() {
+        return hamil;
+    }
+
+    // + getSaldo(): int 
+    public int getSaldo() {
+        return saldo;
+    }
+
+    // Method Mutator (setters/modifiers)
+    // + tambahSaldo(saldobaru: int) 
+    public void tambahSaldo(int saldoBaru) {
+        this.saldo += saldoBaru;
+    }
+
+    // + kurangiSaldo(ongkos: int): void 
+    public void kurangiSaldo(int ongkos) {
+        if (this.saldo >= ongkos) {
+            this.saldo -= ongkos;
+        } else {
+            // Seharusnya sudah dicek sebelum naik, tapi untuk keamanan
+            throw new IllegalArgumentException("Saldo tidak cukup.");
+        }
+    }
+
+    // Tambahan method untuk mengecek status prioritas
+    // Digunakan untuk logika di Class Bus
+    public boolean isPrioritas() {
+        // Lansia: umur lebih dari 60 tahun 
+        // Anak-anak: umur kurang dari 10 tahun 
+        // Ibu hamil 
+        return this.umur > 60 || this.umur < 10 || this.hamil;
+    }
+}
